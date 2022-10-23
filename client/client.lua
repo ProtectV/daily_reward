fcd3b3ffea45ed01f7085b6e22d319962996c09c
Marco <acc@save-bot.eu>
2022-10-23 19:29:06 +0200

    Remove testing code

--- client/client.lua
+++ client/client.lua
@@ -2,24 +2,6 @@ ESX = nil
 
 TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 
-ESX.TriggerServerCallback('daily_reward:check', function(state)
-
-    if state.state then
-
-        if state.item ~= "money" then
-
-            exports["protectv_notify"]:notify("DAILY REWARD", "Du hast ".. state.amount .. " mal ".. state.item .. " bekommen")
-
-        else
-
-            exports["protectv_notify"]:notify("DAILY REWARD", "Du hast $".. state.amount .. " bekommen")
-
-        end
-
-    end
-
-end)
-
 AddEventHandler('playerSpawned', function()
     Citizen.CreateThread(function()
 
@@ -29,7 +11,7 @@ AddEventHandler('playerSpawned', function()
 
                 if state.item ~= "money" then
 
-                    exports["protectv_notify"]:notify("DAILY REWARD", "Du hast ".. state.amount .. "x".. state.item .. " bekommen")
+                    exports["protectv_notify"]:notify("DAILY REWARD", "Du hast ".. state.amount .. " mal ".. state.item .. " bekommen")
 
                 else
 
